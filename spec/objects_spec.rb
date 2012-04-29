8e7386753c8063224e8f76cc6622357e13da0337
Boy Maas <boy.maas@gmail.com>
2012-04-29 16:40:44 +0200

    rubified method calls and abstracted to composable

--- spec/objects_spec.rb
+++ spec/objects_spec.rb
@@ -1,4 +1,4 @@
-class Source
+class Composable
   attr_accessor :children, :parent
 
   def initialize(parent=:root)
@@ -6,16 +6,16 @@ class Source
     @parent = parent
   end
 
-  def addChild(child)
+  def add_child(child)
     child.parent = self
     self.children << child
   end
 
-  alias :<<  :addChild
+  alias :<<  :add_child
 
-  def addChildren(children)
+  def add_children(children)
     children.each do |child|
-      addChild(child)
+      add_child(child)
     end
   end
 
@@ -37,13 +37,17 @@ class Source
   end
 end
 
-class RoxxDefinition < Source
+
+class Sound < Composable
+end
+
+class AudioMix < Composable
 end
 
 class Renderer
 end
 
-describe Source do
+describe Composable do
   context "#children" do
     it "is initialized with an array" do
       subject.children.should == []
@@ -54,10 +58,10 @@ describe Source do
       subject.parent.should == :root
     end
   end
-  context "#addChild" do
-    let(:added_source) {Source.new}
+  context "#add_child" do
+    let(:added_source) {described_class.new}
     let(:root) do
-      subject.addChild(added_source)    
+      subject.add_child(added_source)    
       subject
     end
     it "adds a child" do
@@ -69,31 +73,31 @@ describe Source do
   end
 
   context "#<<" do
-    it "adds children" do
-      root = Source.new
-      root << ( added_source = Source.new )
+    it "adds a child" do
+      root = described_class.new
+      root << ( added_source = described_class.new )
 
       root.children.first.should == added_source 
     end
   end
 
-  context "#addChildren" do
-    it "should call addChild number of times" do
-      subject.should_receive(:addChild).exactly(3).times
-      subject.addChildren([stub,stub,stub])
+  context "#add_children" do
+    it "should call add_child number of times" do
+      subject.should_receive(:add_child).exactly(3).times
+      subject.add_children([stub,stub,stub])
     end
   end
 
   context "walking the tree" do
     def generate_sources count, parent=nil
-      Array.new(count) { Source.new(parent) }
+      Array.new(count) { described_class.new(parent) }
     end
     let(:children0) { generate_sources(2) }
     let(:children1) { generate_sources(2) }
     let(:root) do
-      root = Source.new()
-      root.addChildren( children0 )
-      root.children[0].addChildren( children1 )
+      root = described_class.new()
+      root.add_children( children0 )
+      root.children[0].add_children( children1 )
       root
     end
 
@@ -121,7 +125,7 @@ describe Source do
   end
 end
 
-describe RoxxDefinition do
+describe AudioMix do
   it "has children" do
     subject.children.should == []
   end
