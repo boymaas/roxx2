88485770947565bedd26455c3f07076a65d30e20
Boy Maas <boy.maas@gmail.com>
2012-05-01 00:24:27 +0200

    removed comment

--- spec/lib/roxx/dsl_spec.rb
+++ spec/lib/roxx/dsl_spec.rb
@@ -21,34 +21,6 @@ end
 
 module Roxx::Dsl
 
-  # audio_mix do
-  #   library do
-  #     # no params implies whole file
-  #     audio_file :sound_1, :path => 'path/to/sound.mp3'
-  #     # no duration implies usable untill end
-  #     audio_file :sound_2, :path => 'path/to/sound.mp3', :offset => 10
-  #     # offset and duration 
-  #     audio_file :sound_3, :path => 'path/to/sound.mp3', :offset => 10, :duration => 2.2
-  #   end
-    
-  #   duration 20.minutes
-
-  #   effect :fade_in, :slope => 10.seconds
-  #   effect :fade_out, :slope => 10.seconds
-
-  #   track :voice do
-  #     # no duration will play whole track 
-  #     sound :sound_1
-  #     # duration specified will loop sound_3 for 10 minutes
-  #     sound :sound_3, :duration => 10.minutes
-  #   end
-
-  #   track :background do
-  #     # will repeat until length of audio mix
-  #     sound :sound_3, :duration => :audio_mix
-  #   end
-  # end
-
   describe LibraryCommand do
     context "#audio_file" do
       let(:library) { stub(:library) }
